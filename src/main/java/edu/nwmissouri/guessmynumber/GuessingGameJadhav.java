c7d6b8d04a5a9fae7d166d2abcfe440b3982f0f3
preethi54 <je.preethi@gmail.com>
2021-12-03 15:59:03 -0600

    added class GuessingGameJadhav.java

---
+++ src/main/java/edu/nwmissouri/guessmynumber/GuessingGameJadhav.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.guessmynumber;
+
+import javax.swing.*;
+/**
+ *
+ * @author Preethi Jadhav
+ */
+public class GuessingGameJadhav {
+    public static void main(String[] args) {
+        // TODO: change computerNumber to secretNumber Hint: Right-click / Refactor / Rename
+        int computerNumber = (int) (Math.random() * 100 + 1);
+        int userAnswer = 0;
+        System.out.println("The correct guess would be " + computerNumber);
+        int count = 1;
+
+        while (userAnswer != computerNumber) {
+            // TODO: create 3 local variables for the arguments so we know what they mean
+            // showInputDialog - the first argument is always null (no parent)
+            String message = "Enter a guess between 1 and 100"; // what type?
+            String title = "Guessing Game"; // what type?
+            int messageType = JOptionPane.INFORMATION_MESSAGE;
+            // Works the same as original 3 but more expressive - this is an example of a what? Hint: enum
+            String response = JOptionPane.showInputDialog(null,
+                    "Enter a guess between 1 and 100", "Guessing Game", 3);
+            userAnswer = Integer.parseInt(response);
+
+            JOptionPane.showMessageDialog(null, "" + calculateMessage(userAnswer, computerNumber, count));
+            count++;
+        }
+    }
+
+    // TODO: Refactor / Rename determineGuess to calculateMessage
+    public static String calculateMessage(int userAnswer, int computerNumber, int count) {
+        // TODO: Instead of "Try Number" which sounds like we should try that as a guess...
+        // Say "You've used " + count + guesses.
+        // If count is one, use "guess", else use "guesses" HINT: Ternary operator
+        
+        String s = (count == 1) ? " guess." : " gusses.";
+        if (userAnswer <= 0 || userAnswer > 100) {
+            return "Your guess is invalid";
+        } else if (userAnswer == computerNumber) {
+            return "Correct!\nTotal Guesses: " + count;
+        } else if (userAnswer > computerNumber) {
+            return "Your guess is too high, try again.\nTry Number: " + count +s;
+        } else if (userAnswer < computerNumber) {
+            return "Your guess is too low, try again.\nTry Number: " + count + s;
+        } else {
+            return "Your guess is incorrect\nTry Number: " + count;
+        }
+    }
+}
